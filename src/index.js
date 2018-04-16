eeca691f964dbee44be16f8bf56224ca8deede08
Benoit Schweblin <benoit@lex-persona.com>
2018-04-16 11:25:53 +0100

    Fixed close button color

--- src/index.js
+++ src/index.js
@@ -62,7 +62,6 @@ const styleContent = `
 }
 
 .stackedit-close-button {
-  color: #777;
   position: absolute;
   width: 38px;
   height: 36px;
@@ -88,7 +87,7 @@ const containerHtml = `
   <iframe class="stackedit-iframe"></iframe>
   <a href="javascript:void(0)" class="stackedit-close-button" title="Close">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="100%" height="100%">
-      <path fill="currentColor" d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" />
+      <path fill="#777" d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" />
     </svg>
   </a>
 </div>
